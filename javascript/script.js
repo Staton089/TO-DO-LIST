41cfd258c1a0e81283b5a8b702d25c0e08747a62
rblake089 <rblake089@gmail.com>
2023-08-31 20:41:00 -0400

    Add JavaScript code for adding tasks

--- javascript/script.js
+++ javascript/script.js
@@ -1,26 +1,24 @@
 // Get the input, button, and unordered list elements from the DOM
-let input_El = document.getElementById("input_El"); // Input element for entering tasks
-let add_Btn_El = document.getElementById("add_Btn_El"); // Button element for adding tasks
-let ul_El = document.getElementById("ul_El"); // Unordered list element for displaying tasks
+let input_El = document.getElementById("input_El"); // Get the input element for entering tasks
+let add_Btn_El = document.getElementById("add_Btn_El"); // Get the button element for adding tasks
+let ul_El = document.getElementById("ul_El"); // Get the unordered list element for displaying tasks
 
 // Function to add a new task
 function addTask() {
-    // Get the task text from the input
-    let taskText = input_El.value;
-    
-    if (taskText === "") {
+    // Check if the task text is empty
+    if (input_El.value === "") {
         return; // Do not add empty tasks
     }
     
     // Create a new list item (task)
     let li_El = document.createElement("li");
-    ul_El.appendChild(li_El);
-    li_El.textContent = taskText;
+    ul_El.appendChild(li_El); // Append the new task item to the task list
     
-    // Clear the input field
+    // Set the text content of the new task item to the input value
+    li_El.textContent = input_El.value;
+    
+    // Clear the input field after adding a task
     input_El.value = "";
-    
-    
 }
 
 // Add click event listener to the "Add" button
